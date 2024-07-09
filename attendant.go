37b70e6f9b4638c2d2fa3489c25fbfbc3c2111dd
avishekkumar <avishek.kumar@zeptonow.com>
2024-07-10 00:18:23 +0530

    Adds functionality for attendant to handle multiple parking area

--- attendant.go
+++ attendant.go
@@ -1,9 +1,39 @@
 package parkinglot
 
+import "errors"
+
+type ParkingLots map[*ParkingArea]bool
+
 type Attendant struct {
-	ParkingArea
+	availableParkingArea ParkingLots
 }
 
-func NewAttendant(parkingArea *ParkingArea) *Attendant {
-	return &Attendant{*parkingArea}
+func NewAttendant(parkingArea ...*ParkingArea) *Attendant {
+	attendant := &Attendant{make(ParkingLots)}
+	attendant.addParkingLots(parkingArea...)
+	return attendant
+}
+
+func (a *Attendant) addParkingLots(parkingArea ...*ParkingArea) {
+	for _, area := range parkingArea {
+		a.availableParkingArea[area] = true
+	}
+}
+
+func (a *Attendant) ParkIn(carId string) error {
+	for parkingArea, _ := range a.availableParkingArea {
+		if parkingArea.ParkIn(carId) == nil {
+			return nil
+		}
+	}
+	return errors.New("parking area not found")
+}
+
+func (a *Attendant) ParkOut(carId string) error {
+	for parkingArea, _ := range a.availableParkingArea {
+		if parkingArea.ParkOut(carId) == nil {
+			return nil
+		}
+	}
+	return errors.New("car not found in any parking area")
 }
